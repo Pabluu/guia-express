d595874297f361c453240cfb373fd95cd6697371
Pablo Henrique <pabloph0077@gmail.com>
2022-10-26 11:39:48 -0300

    deletando categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -28,10 +28,32 @@ router.post("/categories/save", (req, res) => {
 router.get('/admin/categories', (req, res) => {
     Category.findAll()
         .then(categories => {
-            console.log(categories);
             res.render('admin/categories/index', { categories: categories });
 
         })
 });
+
+
+router.post('/admin/categories/delete', (req, res) => {
+    let id = req.body.id;
+
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id:id
+                }
+            })
+            .then(() => {
+                res.redirect('/admin/categories')
+            })
+
+        }else{
+            res.redirect('/admin/categories');
+        }
+    }else{
+        res.redirect('/admin/categories');
+    }
+})
 
 module.exports = router;
