9f9fea5434107f1ce35b7c7f01cd908d63a37d9f
Pablo Henrique <pabloph0077@gmail.com>
2022-10-29 12:17:39 -0300

    salvando edição

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -17,7 +17,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect('admin/categories')
+            res.redirect('/admin/categories')
         })
     } else {
         res.redirect('/admin/categories/new')
@@ -72,6 +72,23 @@ router.get('/admin/categories/edit/:id', (req, res) => {
     }).catch(erro => {
         res.redirect('/admin/categories');
     })
+});
+
+
+router.post('/categories/update', (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+
+    Category.update(
+        { title: title, slug: slugify(title) },
+        {
+            where: {
+                id: id
+            }
+        })
+        .then(() => {
+            res.redirect('/admin/categories');
+        })
 })
 
 
