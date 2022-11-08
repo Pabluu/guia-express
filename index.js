a253289ecc3fc7a97d34aa3499391d0384fc026f
Pablo Henrique <pabloph0077@gmail.com>
2022-11-07 22:08:23 -0300

    criando usuarios

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const connection = require('./database/database');
 
 const Article = require('./articles/Article');
 const Category = require('./categories/Category');
+const User = require('./user/User');
+
 
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
+const userController = require('./user/UserController')
 
 // View engine
 app.set('view engine', 'ejs');
@@ -31,6 +34,7 @@ connection
 
 app.use('/', categoriesController);
 app.use('/', articlesController);
+app.use('/', userController);
 
 
 app.get('/', (req, res) => {
