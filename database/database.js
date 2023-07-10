7e9dc793c418ab841f87768daca3bbc9c250037b
Pablo Henrique da Silva Cunha <pabloph0077@gmail.com>
2023-07-10 13:18:14 -0300

    fix: configurando database

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('guiapress', 'pablo', '2107ph', {
+const connection = new Sequelize('guiapress', 'root', '@2107ph', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
